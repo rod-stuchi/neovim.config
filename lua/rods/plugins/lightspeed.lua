43aa8149ac3709c3c1a661619ce6b47bd83a7d21
Rodrigo Stuchi <rod.stuchi@gmail.com>
2022-01-13 11:49:25 -0300

    change defaults lightspeed 'opts' table

--- lua/rods/plugins/lightspeed.lua
+++ lua/rods/plugins/lightspeed.lua
@@ -19,16 +19,16 @@ function Z.setup()
     exit_after_idle_msecs = { unlabeled = 1000, labeled = nil },
 
     -- s/x
-    grey_out_search_area = true,
-    highlight_unique_chars = true,
-    jump_on_partial_input_safety_timeout = 400,
+    jump_to_unique_chars = { safety_timeout = 400 },
     match_only_the_start_of_same_char_seqs = true,
     substitute_chars = { ['\r'] = '¬' },
-    cycle_group_fwd_key = '<space>',
-    cycle_group_bwd_key = '<tab>',
+    special_keys = {
+      next_match_group = '<space>',
+      prev_match_group = '<tab>',
+    },
 
     -- f/t
-    limit_ft_matches = 4,
+    limit_ft_matches = 5,
     repeat_ft_with_target_char = true,
   })
 end
