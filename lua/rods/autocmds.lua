cb49e77848507ec75e3fa7f6a7806d35c7002492
Rodrigo Stuchi <rod.stuchi@gmail.com>
2023-01-19 19:39:32 -0300

    add autocmd: spelllang=en_us git commit message

--- lua/rods/autocmds.lua
+++ lua/rods/autocmds.lua
@@ -23,4 +23,20 @@ augroup rods_highlights
   " highlight ColorColumn88 ctermbg=magenta guibg=#b41158
   " call matchadd('ColorColumn88', '\%88v')
 augroup END
+
 ]])
+
+vim.api.nvim_create_augroup('Git', {})
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = 'COMMIT_EDITMSG',
+  callback = function()
+    vim.cmd 'set spell spelllang=en_us'
+    -- insert mode if line empty
+    vim.api.nvim_win_set_cursor(0, {1, 0})
+    if vim.fn.getline(1) == '' then
+      vim.cmd 'startinsert!'
+    end
+  end,
+  group = 'Git'
+})
+
